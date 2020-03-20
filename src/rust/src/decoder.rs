6e926ee68047cbe79bcb5b628bd97798670d22b4
Zach Shipko <zachshipko@gmail.com>
2020-03-20 15:21:49 -0700

    Update image dependency in rust backend

--- src/rust/src/decoder.rs
+++ src/rust/src/decoder.rs
@@ -1,35 +1,38 @@
 use std::ffi::CStr;
 use std::fs::File;
-use std::{ptr};
+use std::ptr;
 
-use image::{ImageDecoder};
+use image::ImageDecoder;
 
 use crate::*;
 
-fn image_decode<'a, D: ImageDecoder<'a>>(decoder: D, width: *mut u64, height: *mut u64, channels: *mut u8, bits: *mut u16) -> *mut std::ffi::c_void {
-    let color = decoder.colortype();
+fn image_decode<'a, D: ImageDecoder<'a>>(
+    decoder: D,
+    width: *mut u64,
+    height: *mut u64,
+    channels: *mut u8,
+    bits: *mut u16,
+) -> *mut std::ffi::c_void {
+    let color = decoder.color_type();
     let ch = color.channel_count();
     let b = color.bits_per_pixel() / ch as u16;
     let (w, h) = decoder.dimensions();
 
     unsafe {
-        *width = w;
-        *height = h;
+        *width = w as u64;
+        *height = h as u64;
         *channels = ch;
         *bits = b;
     }
 
-    let data = match decoder.read_image() {
-        Ok(data) => {
+    let mut data = Vec::new();
+    match decoder.read_image(&mut data) {
+        Ok(()) => {
             let data = data.into_boxed_slice();
             Box::into_raw(data) as *mut std::ffi::c_void
         }
-        Err(_) => {
-            return ptr::null_mut();
-        }
-    };
-
-    return data;
+        Err(_) => ptr::null_mut(),
+    }
 }
 
 fn open_file(path: *const i8) -> Option<File> {
@@ -40,19 +43,24 @@ fn open_file(path: *const i8) -> Option<File> {
         Err(_) => return None,
     };
 
-
     let f = match File::open(filename) {
         Ok(f) => f,
         Err(_) => return None,
     };
 
-    return Some(f)
+    return Some(f);
 }
 
 macro_rules! decoder {
     ($name:ident, $dec:expr) => {
         #[no_mangle]
-        pub extern "C" fn $name(path: *const i8, width: *mut u64, height: *mut u64, channels: *mut u8, bits: *mut u16) -> *mut std::ffi::c_void {
+        pub extern "C" fn $name(
+            path: *const i8,
+            width: *mut u64,
+            height: *mut u64,
+            channels: *mut u8,
+            bits: *mut u16,
+        ) -> *mut std::ffi::c_void {
             let f = match open_file(path) {
                 Some(f) => f,
                 None => return ptr::null_mut(),
@@ -67,19 +75,23 @@ macro_rules! decoder {
 
             return image_decode(decoder, width, height, channels, bits);
         }
-    }
+    };
 }
 
-decoder!(image_read_tiff, image::tiff::TIFFDecoder::new);
-decoder!(image_read_jpeg, image::jpeg::JPEGDecoder::new);
-decoder!(image_read_png, image::png::PNGDecoder::new);
+decoder!(image_read_tiff, image::tiff::TiffDecoder::new);
+decoder!(image_read_jpeg, image::jpeg::JpegDecoder::new);
+decoder!(image_read_png, image::png::PngDecoder::new);
 decoder!(image_read_hdr, image::hdr::HDRAdapter::new);
-decoder!(image_read_gif, image::gif::Decoder::new);
-decoder!(image_read_bmp, image::bmp::BMPDecoder::new);
-decoder!(image_read_tga, image::tga::TGADecoder::new);
+decoder!(image_read_gif, image::gif::GifDecoder::new);
+decoder!(image_read_bmp, image::bmp::BmpDecoder::new);
+decoder!(image_read_tga, image::tga::TgaDecoder::new);
 
 #[no_mangle]
-pub extern "C" fn ezimage_imread(path: *const i8, _t: *const Type, shape: *mut Shape) -> *mut std::ffi::c_void {
+pub extern "C" fn ezimage_imread(
+    path: *const i8,
+    _t: *const Type,
+    shape: *mut Shape,
+) -> *mut std::ffi::c_void {
     if path.is_null() || shape.is_null() {
         return ptr::null_mut();
     }
@@ -101,16 +113,58 @@ pub extern "C" fn ezimage_imread(path: *const i8, _t: *const Type, shape: *mut S
     shape.t.kind = Kind::UInt;
 
     match ext {
-        Some("tiff") | Some("TIFF") | Some("tif") | Some("TIF") => image_read_tiff(path, &mut shape.width, &mut shape.height, &mut shape.channels, &mut shape.t.bits),
-        Some("jpeg") | Some("JPEG") | Some("jpg") | Some("JPG") => image_read_jpeg(path, &mut shape.width, &mut shape.height, &mut shape.channels, &mut shape.t.bits),
-        Some("png") | Some("PNG")  => image_read_png(path, &mut shape.width, &mut shape.height, &mut shape.channels, &mut shape.t.bits),
-        Some("gif") | Some("GIF")  => image_read_gif(path, &mut shape.width, &mut shape.height, &mut shape.channels, &mut shape.t.bits),
+        Some("tiff") | Some("TIFF") | Some("tif") | Some("TIF") => image_read_tiff(
+            path,
+            &mut shape.width,
+            &mut shape.height,
+            &mut shape.channels,
+            &mut shape.t.bits,
+        ),
+        Some("jpeg") | Some("JPEG") | Some("jpg") | Some("JPG") => image_read_jpeg(
+            path,
+            &mut shape.width,
+            &mut shape.height,
+            &mut shape.channels,
+            &mut shape.t.bits,
+        ),
+        Some("png") | Some("PNG") => image_read_png(
+            path,
+            &mut shape.width,
+            &mut shape.height,
+            &mut shape.channels,
+            &mut shape.t.bits,
+        ),
+        Some("gif") | Some("GIF") => image_read_gif(
+            path,
+            &mut shape.width,
+            &mut shape.height,
+            &mut shape.channels,
+            &mut shape.t.bits,
+        ),
         Some("hdr") | Some("HDR") => {
             shape.t.kind = Kind::Float;
-            image_read_hdr(path, &mut shape.width, &mut shape.height, &mut shape.channels, &mut shape.t.bits)
-        },
-        Some("bmp") | Some("BMP") => image_read_bmp(path, &mut shape.width, &mut shape.height, &mut shape.channels, &mut shape.t.bits),
-        Some("tga") | Some("TGA") => image_read_tga(path, &mut shape.width, &mut shape.height, &mut shape.channels, &mut shape.t.bits),
+            image_read_hdr(
+                path,
+                &mut shape.width,
+                &mut shape.height,
+                &mut shape.channels,
+                &mut shape.t.bits,
+            )
+        }
+        Some("bmp") | Some("BMP") => image_read_bmp(
+            path,
+            &mut shape.width,
+            &mut shape.height,
+            &mut shape.channels,
+            &mut shape.t.bits,
+        ),
+        Some("tga") | Some("TGA") => image_read_tga(
+            path,
+            &mut shape.width,
+            &mut shape.height,
+            &mut shape.channels,
+            &mut shape.t.bits,
+        ),
         _ => {
             let mut r = std::ptr::null_mut();
 
@@ -123,14 +177,18 @@ pub extern "C" fn ezimage_imread(path: *const i8, _t: *const Type, shape: *mut S
                 image_read_tga,
             ];
             for f in attempt {
-                r = f(path, &mut shape.width, &mut shape.height, &mut shape.channels, &mut shape.t.bits);
-                if !r.is_null(){
-                    break
+                r = f(
+                    path,
+                    &mut shape.width,
+                    &mut shape.height,
+                    &mut shape.channels,
+                    &mut shape.t.bits,
+                );
+                if !r.is_null() {
+                    break;
                 }
             }
             return r;
         }
     }
 }
-
-
--- src/rust/src/encoder.rs
+++ src/rust/src/encoder.rs
@@ -3,9 +3,13 @@ use std::ffi::CStr;
 use crate::*;
 
 #[no_mangle]
-pub extern "C" fn ezimage_imwrite(path: *const i8, data: *const std::ffi::c_void, shape: *const Shape) -> bool {
+pub extern "C" fn ezimage_imwrite(
+    path: *const i8,
+    data: *const std::ffi::c_void,
+    shape: *const Shape,
+) -> bool {
     if path.is_null() || data.is_null() || shape.is_null() {
-        return false
+        return false;
     }
 
     let filename = unsafe { CStr::from_ptr(path) };
@@ -14,30 +18,31 @@ pub extern "C" fn ezimage_imwrite(path: *const i8, data: *const std::ffi::c_void
         Err(_) => return false,
     };
 
-    let shape = unsafe {&*shape};
+    let shape = unsafe { &*shape };
 
     let size = shape.width * shape.height * shape.channels as u64 * shape.t.bits as u64 / 8;
     let slice = unsafe { std::slice::from_raw_parts(data as *const u8, size as usize) };
 
-    let color = match shape.channels {
-        1 => {
-            image::ColorType::Gray(shape.t.bits as u8)
-        }
-        2 => {
-            image::ColorType::GrayA(shape.t.bits as u8)
-        }
-        3 => {
-            image::ColorType::RGB(shape.t.bits as u8)
-        }
-        4 => {
-            image::ColorType::RGBA(shape.t.bits as u8)
-        }
-        _ => return false
+    let color = match (shape.channels, shape.t.bits) {
+        (1, 8) => image::ColorType::L8,
+        (1, 16) => image::ColorType::L16,
+        (2, 8) => image::ColorType::La8,
+        (2, 16) => image::ColorType::La16,
+        (3, 8) => image::ColorType::Rgb8,
+        (3, 16) => image::ColorType::Rgb16,
+        (4, 8) => image::ColorType::Rgba8,
+        (4, 16) => image::ColorType::Rgba16,
+        _ => return false,
     };
 
-    match image::save_buffer(filename, slice, shape.width as u32, shape.height as u32, color) {
+    match image::save_buffer(
+        filename,
+        slice,
+        shape.width as u32,
+        shape.height as u32,
+        color,
+    ) {
         Ok(()) => true,
         Err(_) => false,
     }
 }
-
